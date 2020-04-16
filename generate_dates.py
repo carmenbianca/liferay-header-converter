507698d5d04f3184daa4d99555132267478e5a79
Carmen Bianca Bakker <carmenbianca.bakker@liferay.com>
2020-04-16 15:03:06 +0100

    No longer generate json

--- generate_dates.py
+++ generate_dates.py
@@ -2,28 +2,15 @@
 #
 # SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
 
-import json
 import subprocess
-import os
-
-from liferay_header_converter import all_files, get_latest_date, ROOT_PATH
-
 
 def main():
-    dates = dict()
-
     # Remove all untracked files
     subprocess.run(["git", "clean", "-dxf"])
     # Change the mtime of all files to the last time they were modified. Through
     # some voodoo in `restore-mtime`, this is much quicker than requesting the
     # last modified time via `git log`.
     subprocess.run(["git", "restore-mtime"])
-
-    for file_ in all_files():
-        dates[file_] = get_latest_date(file_).isoformat()
-
-    with open(os.path.join(ROOT_PATH, "dates.json"), "w") as fp:
-        json.dump(dates, fp, indent=1)
 
 
 if __name__ == "__main__":
